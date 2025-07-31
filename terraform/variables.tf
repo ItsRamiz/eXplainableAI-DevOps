022e8428780e4c489abb48b3ec7e51d5b295b2c8
Ramiz Srour <100355462+ItsRamiz@users.noreply.github.com>
2025-07-31 23:51:56 +0300

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,3 +2,29 @@ variable "aws_region" {
   description = "AWS Deployment Region"
   type        = string
 }
+
+variable "bucket_name" {
+  description = "S3 bucket name for static files"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "ami_id" {
+  description = "AMI ID for EC2 instances"
+  type        = string
+}
+
+variable "key_name" {
+  description = "Name of existing key pair"
+  type        = string
+}
+
+variable "public_key_path" {
+  description = "Path to public SSH key"
+  type        = string
+}
